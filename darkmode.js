76cfc479277fd33cc1735dc1827caefa992d75c2
LeL-nardo <sandroleonardoads@gmail.com>
2022-09-08 22:44:45 -0300

    Avançando no projeto
    criei um layout base para o anúncio de produtos

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,7 @@
 function darkmode() {
     let botao = document.getElementById("toggle_theme")
     if (botao.checked) {
-        document.body.style.background = "#121212"
+        document.body.style.background = "#18191A"
     } else {
         document.body.style.background = "white"
     }
