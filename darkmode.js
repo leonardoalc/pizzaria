7aa417fc14d7c37143cc6c87fbfb214eda67ed0f
LeL-nardo <sandroleonardoads@gmail.com>
2022-09-12 17:51:54 -0300

    pizzaria v1.0
    "Finalizei" a pizzaria, irei alterar as medias queries e ajustes de tela conforme for avançando.

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,6 @@
-function darkmode() {
-    let botao = document.getElementById("toggle_theme")
-    if (botao.checked) {
-        document.body.style.background = "#18191A"
-    } else {
-        document.body.style.background = "white"
-    }
-}
+let html = document.querySelector("html")
+let toggle = document.getElementById("toggle_theme")
+
+toggle.addEventListener("change", () => {
+    html.classList.toggle("dark_mode")
+})
